7242d85bd35d713887c5e32e75ef06a5722eaca8
Tomás Fernández <tomas.fernandez@greencodesoftware.com>
2018-05-29 19:05:59 -0300

    chat anda

---
+++ config/environments/all.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = {
+    redis: 'redis://192.168.99.100:6379',
+    maxMessageCache: 50,
+    port: 8080,
+    version: require('../../package.json').version,
+    socketUrl: 'http://192.168.99.100'
+};
+//# sourceMappingURL=all.js.map
---
+++ config/environments/all.ts
@@ -0,0 +1,9 @@
+import {IEnv} from "./IEnv";
+
+export =<IEnv> {
+    redis:'redis://192.168.99.100:6379',
+    maxMessageCache:50,
+    port : 8080,
+    version:require('../../package.json').version,
+    socketUrl:'http://192.168.99.100'
+}
