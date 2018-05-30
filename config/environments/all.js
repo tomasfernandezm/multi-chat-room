f44072614d62a11825cc541bf6fd279e1c1c9628
Tomás Fernández <tomas.fernandez@greencodesoftware.com>
2018-05-30 16:55:55 -0300

    commit

--- config/environments/all.js
+++ config/environments/all.js
@@ -4,6 +4,6 @@ module.exports = {
     maxMessageCache: 50,
     port: 8080,
     version: require('../../package.json').version,
-    socketUrl: 'http://192.168.99.100'
+    socketUrl: 'http://localhost:8080'
 };
 //# sourceMappingURL=all.js.map
--- config/environments/all.ts
+++ config/environments/all.ts
@@ -5,5 +5,5 @@ export =<IEnv> {
     maxMessageCache:50,
     port : 8080,
     version:require('../../package.json').version,
-    socketUrl:'http://192.168.99.100'
+    socketUrl:'http://localhost:8080'
 }
